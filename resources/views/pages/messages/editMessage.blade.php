5281ffd363dde8c6a290e1af3624a9e7c9f1cd68
paleksa95 <paleksa95@gmail.com>
2017-07-07 00:03:30 +0100

    Edit message form.

---
+++ resources/views/pages/messages/editMessage.blade.php
@@ -0,0 +1,44 @@
+@extends('main.main')
+
+@section('Title', 'Edit Message')
+
+@section('content')
+
+    @include('parts.session')
+
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+
+
+
+                @if($message)
+                <h1>Edit Message</h1>
+
+                <hr>
+                <form action="{{ route('editMessage') }}" method="POST">
+                    <div class="form-group">
+
+                        <div class="form-group">
+                            <label name="body">Message body:</label>
+                        <textarea id="body" name="body" value="{{$message->body}}" class="form-control"
+                                 >{{$message->body}}</textarea>
+                        </div>
+                        {{ csrf_field() }}
+                        <input type="submit" value="Update Message" class="btn btn-lg btn-primary btn-bloc">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@else
+    <center>
+        <h1>No message found</h1>
+    </center>
+@endif
+
+@endsection
